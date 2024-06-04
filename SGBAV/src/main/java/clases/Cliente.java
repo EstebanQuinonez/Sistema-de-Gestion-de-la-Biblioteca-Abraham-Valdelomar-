9da6e3be2aa0d9d1900f421584f469601de4739e
OscarMacchiavello <mapeoscar123@gmail.com>
2024-06-03 19:23:28 -0500

    Cambio de nombres en archivos (de usuario a cliente).

---
+++ SGBAV/src/main/java/clases/Cliente.java
@@ -0,0 +1,58 @@
+package clases;
+
+import java.util.Scanner;
+
+public class Cliente {
+    private int ID;
+    private String nombre;
+    private String correo;
+
+    public Cliente(int ID, String nombre, String apellido, String correo) {
+        this.ID = ID;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    // Getters and setters
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public static void verCliente(Cliente cliente) {
+        System.out.println("El ID del cliente es: " + cliente.getID());
+        System.out.println("El nombre del cliente es: " + cliente.getNombre());
+        System.out.println("El correo del cliente es: " + cliente.getCorreo());
+    }
+
+    public static Cliente buscarClientePorID(int ID, ListaDoblementeEnlazada<Cliente> listaClientes) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el ID a buscar: ");
+
+    }
+
+    public static Cliente eliminarClientePorID(int ID, ListaDoblementeEnlazada<Cliente> listaClientes) {
+
+    }
+
+    public static void agregarCliente(int ID, String nombre, String correo, ListaDoblementeEnlazada<Cliente> listaClientes) {
+       // se crea objeco cliente y se devuelve: void --> Cliente
+    }
+    public static void verListaDeClientes(ListaDoblementeEnlazada<Cliente> listaClientes) {
+
+    }
+}
---
+++ SGBAV/src/main/java/com/mycompany/sgbav/MenuGestionDeClientes.java
@@ -0,0 +1,146 @@
+package com.mycompany.sgbav;
+
+import clases.Cliente;
+import clases.Libro;
+import clases.ListaDoblementeEnlazada;
+
+import java.util.Scanner;
+
+public class MenuGestionDeClientes {
+
+    public static void menuGestionDeClientes(ListaDoblementeEnlazada<Libro> listaLibros, ListaDoblementeEnlazada<Cliente> listaClientes) {
+        System.out.println("\n***Gestion de clientes***");
+        System.out.println("1. Buscar cliente.");
+        System.out.println("2. Eliminar cliente.");
+        System.out.println("3. Agregar cliente.");
+        System.out.println("4. Ver lista de clientes.");
+        System.out.println("5. Volver.");
+        System.out.println("6. Salir.");
+
+        int opcion;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Seleccione una opcion: ");
+            opcion = scanner.nextInt();
+            if(opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5 && opcion != 6) {
+                System.out.println("Elija una opcion válida!");
+            }
+        } while(opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5 && opcion != 6);
+
+        switch (opcion)
+        {
+            case 1:
+                menuBuscarCliente(listaLibros, listaClientes);
+
+                break;
+            case 2:
+                menuEliminarCliente(listaLibros, listaClientes);
+
+                break;
+            case 3:
+                Cliente.agregarCliente();
+
+                break;
+            case 4:
+                // verListaDeUsuarios();
+                break;
+            case 5:
+                MenuPrincipal.menuPrincipal(listaLibros, listaClientes);
+
+                break;
+            case 6:
+                System.exit(0);
+        }
+    }
+
+    public static void menuBuscarCliente(ListaDoblementeEnlazada<Libro> listaLibros, ListaDoblementeEnlazada<Cliente> listaClientes) {
+        System.out.println("\n***Menu de búsqueda de clientes***");
+        System.out.println("1. Buscar cliente por ID.");
+        System.out.println("2. Buscar cliente por nombre.");
+        System.out.println("3. Buscar cliente por correo.");
+        System.out.println("4. Volver.");
+        System.out.println("5. Salir.");
+
+        int opcion;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Seleccione una opcion: ");
+            opcion = scanner.nextInt();
+            if(opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5) {
+                System.out.println("Elija una opcion válida!");
+            }
+        } while(opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5);
+
+        switch (opcion)
+        {
+            case 1:
+                System.out.println("Ingrese el ID a buscar: ");
+                int ID = scanner.nextInt();
+                Cliente.buscarClientePorID(ID, listaClientes);
+
+                break;
+            case 2:
+                // Cliente.buscarClientePorNombre()
+
+                break;
+            case 3:
+                // Cliente.buscarClientePorCorreo()
+
+                break;
+            case 4:
+                MenuGestionDeClientes.menuGestionDeClientes(listaLibros, listaClientes);
+
+                break;
+            case 5:
+                System.exit(0);
+        }
+    }
+
+    public static void menuEliminarCliente(ListaDoblementeEnlazada<Libro> listaLibros, ListaDoblementeEnlazada<Cliente> listaClientes) {
+
+        System.out.println("\n***Menu de búsqueda de clientes***");
+        System.out.println("1. Eliminar cliente por ID.");
+        System.out.println("2. Eliminar cliente por nombre.");
+        System.out.println("3. Eliminar cliente por correo.");
+        System.out.println("4. Volver.");
+        System.out.println("5. Salir.");
+
+        int opcion;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Seleccione una opcion: ");
+            opcion = scanner.nextInt();
+            if(opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5) {
+                System.out.println("Elija una opcion válida!");
+            }
+        } while(opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5);
+
+        switch (opcion)
+        {
+            case 1:
+                System.out.println("Ingrese el ID a buscar: ");
+                int ID = scanner.nextInt();
+                Cliente.eliminarClientePorID(ID, listaClientes);
+
+                break;
+            case 2:
+                // Cliente.eliminarClientePorNombre()
+
+                break;
+            case 3:
+                // Cliente.eliminarClientePorCorreo()
+
+                break;
+            case 4:
+                MenuGestionDeClientes.menuGestionDeClientes(listaLibros, listaClientes);
+
+                break;
+            case 5:
+                System.exit(0);
+        }
+    }
+    // public static void agregarCliente() {}
+
+    // Probablemente este método sea implementado directamente en la clase usuario y tenga que ser static para poder llamarlo sin crear objeto
+    // public void verListaDeUsuarios() {}
+}
